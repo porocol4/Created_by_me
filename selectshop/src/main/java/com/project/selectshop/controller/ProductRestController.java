1f129f95ebe002aeffeacbefc932b1c1a4c7b938
porocol4 <77235858+porocol4@users.noreply.github.com>
2021-07-22 12:21:45 +0900

    Add Project [select shop]

---
+++ selectshop/src/main/java/com/project/selectshop/controller/ProductRestController.java
@@ -0,0 +1,40 @@
+package com.project.selectshop.controller;
+
+import com.project.selectshop.models.Product;
+import com.project.selectshop.models.ProductMypriceRequestDto;
+import com.project.selectshop.models.ProductRepository;
+import com.project.selectshop.models.ProductRequestDto;
+import com.project.selectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
+@RestController // JSON으로 데이터를 주고받음을 선언합니다.
+public class ProductRestController {
+
+    private final ProductRepository productRepository;
+    private final ProductService productService;
+
+
+    // 등록된 전체 상품 목록 조회
+    @GetMapping("/api/products")
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) {
+        Product product = new Product(requestDto);
+        return productRepository.save(product);
+    }
+
+
+    @PutMapping("/api/products/{id}")
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto){
+        return productService.update(id, requestDto);
+    }
+
+
+}
